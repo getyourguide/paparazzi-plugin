e4c6dd090948bac407d91e5bfcc5c2c2dba37428
thsaravana <th.saravana@gmail.com>
2022-11-05 21:27:30 +0100

    Simplify RefreshAction

--- src/main/kotlin/com/getyourguide/paparazzi/actions/RefreshAction.kt
+++ src/main/kotlin/com/getyourguide/paparazzi/actions/RefreshAction.kt
@@ -1,20 +1,13 @@
 package com.getyourguide.paparazzi.actions
 
+import com.getyourguide.paparazzi.loadFromSelectedEditorFile
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.getyourguide.paparazzi.service.service
 
 
 class RefreshAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project = e.project
-        if (project != null) {
-            val file = FileEditorManager.getInstance(project)?.selectedEditor?.file
-            if (file != null) {
-                e.project?.service?.reload(file)
-            }
-        }
+        e.project?.loadFromSelectedEditorFile()
     }
 }
