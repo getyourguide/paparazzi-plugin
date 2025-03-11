61fe14f308fc38bea2e14edca99153caa16afc60
Clara Hille <79694873+m0ana@users.noreply.github.com>
2025-03-11 17:54:41 +0100

    Add token management and update workflows.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,21 @@
+pluginManagement {
+  repositories {
+    apply(from = "${rootProject.projectDir}/gradle/utils/token-utils.gradle")
+    val fetchToken = (extra["fetchCodeArtifactTokenMethod"] as groovy.lang.Closure<*>).call() as String
+    maven {
+      name = "codeartifact"
+      url =
+              uri("https://getyourguide-130607246975.d.codeartifact.eu-central-1.amazonaws.com/maven/private/")
+      credentials {
+        username = "aws"
+        password = fetchToken
+      }
+    }
+  }
+}
 
 plugins {
-    id 'com.getyourguide.libs.gradle.develocity.configuration' version '4.14.2'
+    id("com.getyourguide.libs.gradle.develocity.configuration") version "4.14.2"
 }
 
 rootProject.name = "paparazzi"
